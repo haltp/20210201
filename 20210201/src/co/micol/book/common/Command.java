c68f9e5fdb35c63fc88e96d675ef2e5c39cde3ff
nklee <namoo3605@naver.com>
2021-02-01 15:21:35 +0900

    s

--- 20210201/src/co/micol/book/common/Command.java
+++
@@ -1,8 +0,0 @@
-package co.micol.book.common;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface Command {
-	public String exec(HttpServletRequest requset, HttpServletResponse response);
-}
